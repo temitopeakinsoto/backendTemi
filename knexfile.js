8ffd72c86c957f0e99f510ca3c525163d634d2df
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-18 14:31:13 +0100

    Initialize project files and install npm dependencies

---
+++ knexfile.js
@@ -0,0 +1,43 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/database.db3',
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    }, 
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test-database.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    }, 
+  },
+
+  production: {
+    
+  }
+};
