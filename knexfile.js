cabbad320ecd5b957398900821066700823fbb2e
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-18 14:40:19 +0100

    Add production environment to knexfile configurations

--- knexfile.js
+++ knexfile.js
@@ -38,6 +38,14 @@ module.exports = {
   },
 
   production: {
-    
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   }
 };
