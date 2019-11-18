a23996e04890a923fd7f8b99cb0d2111b423b19a
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-18 17:41:56 +0100

    Add the correct path to db-config file

--- data/seeds/02-create-users-data.js
+++ data/seeds/02-create-users-data.js
@@ -4,15 +4,15 @@ exports.seed = function(knex) {
     .then(function() {
       return knex("users").insert([
         {
-          username: "temitope-bpa-app"
+          username: "temitope-bpa-app",
           password: "test123"
         },
         {
-          username: "carnun-bpa-app"
+          username: "carnun-bpa-app",
           password: "test123"
         },
         {
-          username: "dimeji-bpa-app"
+          username: "dimeji-bpa-app",
           password: "test123"
         }
       ]);
