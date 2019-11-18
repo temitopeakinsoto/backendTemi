7bbceace5a3c07908d4641345328f4a982975ca4
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-18 14:54:49 +0100

    Add barebone server.js file
    In this commit, I added a barebone server.js file for which will be used for routing client requests.
    This file contains a test [GET] / for testing if our server is up and running

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.json({ api: 'Yaay! our api is working' });
+});
+
+module.exports = server;
