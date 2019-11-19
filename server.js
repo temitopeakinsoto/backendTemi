f7df5d22dcd1667e9a6f084b7d8f791f4e7f9fe8
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-19 21:34:34 +0100

    Add project router to our server.js file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const studentsRouter = require('./api/students/students-router.js');
 const messagesRouter = require('./api/messages/messages-router.js');
+const ProjectsRouter = require('./api/projects/projects-router.js');
 const authRouter = require('./api/auth/auth-router');
 
 const server = express();
@@ -17,5 +18,6 @@ server.use(cors());
 server.use('/api/auth', authRouter);
 server.use('/api/students', studentsRouter);
 server.use('/api/messages', messagesRouter);
+server.use('/api/projects', ProjectsRouter);
 
 module.exports = server;
