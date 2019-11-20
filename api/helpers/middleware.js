c362eebe5bdcd5a3bc6f5577e257913a8bc9f9c0
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-20 12:41:36 +0100

    Ädd a restricted() middleware for protecting/authenticating requests to all resources endpoints

--- api/helpers/middleware.js
+++ api/helpers/middleware.js
@@ -1,11 +1,29 @@
 const Users = require("../students/students-model");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   validateStudent,
   validateStudentId,
   validateNewUser,
-  validateMessage
+  validateMessage,
+  restricted
 };
+
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: err.message });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+}
 
 function validateNewUser(req, res, next) {
   let user = req.body;
